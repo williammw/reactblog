a017ba8d63d85d4b15fff000ab0ef21af42c461d
Man Wai Wong <william.manwai@gmail.com>
2019-01-25 01:22:55 +0800

    reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,10 @@
-export default (state,action) => {
-    
-    //bad
-    //return document.querySelector('input');
-    // bad
-    //return axios.get('/posts');
+export default (state = [],action) => {
+    switch (action.type){
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
+
+    return state;
 }
